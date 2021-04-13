6f809e4cb0c2b4495f875f6118b7059e10881813
geraldarzy <geraldarzola@gmail.com>
2021-04-13 15:51:21 -0400

    commit tree bdb6dfb8e5a04b8dbf352ca322c3e44211e9361b

--- src/ExampleComponent.js
+++ src/ExampleComponent.js
@@ -5,7 +5,7 @@ class ExampleComponent extends Component {
   render() {
     return (
       <div className="example">
-        <img src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif" alt="a deeply impressed man, perhaps thinking 'whoa'"/>
+        <img src="https://media1.tenor.com/images/01dc2ea52594e9a50d5b39355f771356/tenor.gif?itemid=17715186" alt="a deeply impressed man, perhaps thinking 'whoa'"/>
         <p>Whoa!</p>
       </div>
     );
